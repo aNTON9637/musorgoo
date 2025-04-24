f3f0816d3727e39832cf7fc420c421143fb20d0e
Маргошкин <margoskin@MacBook-Margoskin.local>
2025-04-24 04:29:32 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Сайт MusorGo успешно загружен!');
+    
+    // Здесь можно добавить дополнительную функциональность
+}); 
